6f408aa67dfe14f6953ffb0e8657dfff90fd5f78
Maxim-rrrr <61673530+Maxim-rrrr@users.noreply.github.com>
2021-02-23 16:56:27 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    'bot_name': 'say_english_bot',
-    'bot_token': '1515360072:AAHJR53_p3sn8QtsdosLb_TOx4EWgqPQCEQ',
-
-    'admin_password': '123',
-
-    'mongo_connect': 'mongodb+srv://db:cR8zITR9WwEG29r4@cluster0.b3t5o.mongodb.net/<dbname>?retryWrites=true&w=majority'
-}
